831990cc73b1954d200264823886a212981fd537
indelvishwakarma <31717910+indelvishwakarma@users.noreply.github.com>
2017-11-28 15:20:20 -0500

    Spring Boot 12 Factor Application

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,19 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class WelcomeController {
+public class WelcomeController{
+    private String WELCOME_MESSAG;
 
+    public WelcomeController( @Value("${WELCOME_MESSAGE}") String msg){
+        this.WELCOME_MESSAG = msg;
+    }
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return WELCOME_MESSAG;
     }
 }
